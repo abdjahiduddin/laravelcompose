5abc9b3479b7b0d683c53fe9f08f0772e646c179
root <root@ip-172-31-80-113.ec2.internal>
2020-10-26 09:49:49 +0000

    Update init.sql

---
+++ query/init.sql
@@ -0,0 +1,12 @@
+USE laravel;
+
+DROP TABLE IF EXISTS `dblist`;
+
+CREATE TABLE `dblist` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `dblist` (name, status) VALUES ('laravel','connected');
